4cc16349acef10351021cbe29a16fc249806b9a7
map32 <freepina@gmail.com>
2025-05-30 19:15:17 +0900

    wow

--- agriculture-korea/app/index.tsx
+++ agriculture-korea/app/index.tsx
@@ -1,4 +1,31 @@
 import { Text, View } from "react-native";
+import WebView from "react-native-webview";
+
+const html = `
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=e0d156f68eeeb86d84e356eda06761ba&libraries=services,clusterer,drawing"></script> 
+    </head>
+    <body >
+        <div id="map" style="width:500px;height:400px;"></div>
+        <script type="text/javascript">
+            (function () {
+                const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+                const options = { //지도를 생성할 때 필요한 기본 옵션
+                    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+                    level: 3 //지도의 레벨(확대, 축소 정도)
+                };
+                
+                const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+                
+                // 주소-좌표 변환 객체를 생성합니다
+                const geocoder = new kakao.maps.services.Geocoder();
+            })();
+        </script>       
+    </body>
+</html>    
+`;
 
 export default function Index() {
   return (
@@ -9,7 +36,13 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <WebView
+        source={{ html: html }}
+        style={{ width: 500, height: 400 }}
+        javaScriptEnabled={true}
+        >
+
+      </WebView>
     </View>
   );
 }
