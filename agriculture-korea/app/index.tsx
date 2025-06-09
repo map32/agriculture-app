d294b9dc2eb5cf9731b701ac0703ba18f9a741a8
map32 <freepina@gmail.com>
2025-06-09 19:14:15 +0900

    a

--- agriculture-korea/app/index.tsx
+++ agriculture-korea/app/index.tsx
@@ -1,48 +1,104 @@
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView } from "react-native";
 import WebView from "react-native-webview";
+import { useAssets } from 'expo-asset';
+import {readAsStringAsync} from 'expo-file-system';
+import { Dimensions } from 'react-native';
+import { useState, useRef } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const html = `
-<html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=e0d156f68eeeb86d84e356eda06761ba&libraries=services,clusterer,drawing"></script> 
-    </head>
-    <body >
-        <div id="map" style="width:500px;height:400px;"></div>
-        <script type="text/javascript">
-            (function () {
-                const container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-                const options = { //지도를 생성할 때 필요한 기본 옵션
-                    center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-                    level: 3 //지도의 레벨(확대, 축소 정도)
-                };
-                
-                const map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-                
-                // 주소-좌표 변환 객체를 생성합니다
-                const geocoder = new kakao.maps.services.Geocoder();
-            })();
-        </script>       
-    </body>
-</html>    
-`;
+const getPolygons = async () => {
+  const polygons = await AsyncStorage.getItem('polygons');
+  if (polygons) {
+    console.log('Polygons found:', polygons);
+    return JSON.parse(polygons);
+  }
+  console.log('No polygons found, returning empty array');
+  return [];
+}
+
+const addPolygon = async (polygon: any) => {
+  const polygons = await getPolygons();
+  polygons.push(polygon);
+  await AsyncStorage.setItem('polygons', JSON.stringify(polygons));
+}
+
+const removePolygon = async (polygon: any) => {
+  const polygons = await getPolygons();
+  const index = polygons.findIndex((p: any) => p.id === polygon.id);
+  if (index !== -1) {
+    polygons.splice(index, 1);
+    await AsyncStorage.setItem('polygons', JSON.stringify(polygons));
+  }
+}
 
 export default function Index() {
+  const windowHeight = Dimensions.get('window').height;
+  const width = Dimensions.get('window').width;
+  const [index, err] = useAssets(require('@/assets/map.html'));
+  const webviewRef = useRef<WebView>(null); // Correctly typed ref for WebView
+  const [html, setHtml] = useState<string | null>(null);
+  if (index && index[0].localUri) {
+    readAsStringAsync(index[0].localUri).then((data) => {
+        setHtml(data);
+    });
+  }
+  const handleMessage = async (event: any) => {
+    const {data, type} = JSON.parse(event.nativeEvent.data);
+    console.log('Received message from WebView:', data, type);
+    if (type === 'polygon') {
+      addPolygon(data).then(() => {
+        console.log('Polygon added:', data);
+      }
+      ).catch((error) => {
+        console.error('Error adding polygon:', error);
+      }
+      );
+    } else if (type === 'ready') {
+      const run = `
+        (function () {
+        try {
+          const event = new CustomEvent('initpolygons', {detail: ${JSON.stringify(await getPolygons())}});
+          document.dispatchEvent(event);
+        }
+        catch (error) {
+         const newDiv = document.createElement('div');
+                            newDiv.style.position = 'absolute';
+                            newDiv.style.top = '10px';
+                            newDiv.style.left = '10px';
+                            newDiv.style.background = 'rgba(255,255,255,0.8)';
+                            newDiv.style.padding = '8px';
+                            newDiv.style.borderRadius = '4px';
+                            newDiv.style.zIndex = '10';
+                            newDiv.style.minWidth = '200px';
+                            newDiv.style.maxWidth = '400px';
+                            newDiv.style.minHeight = '50px';
+                            newDiv.textContent = error.message;
+                            document.body.appendChild(newDiv);
+}
+        })();
+      `;
+      console.log('WebView is ready, injecting polygons', run);
+      webviewRef.current?.injectJavaScript(run);
+    } else if (type === 'polygon-click') {
+      
+    }
+  }
   return (
-    <View
+    <SafeAreaView
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
       }}
     >
       <WebView
-        source={{ html: html }}
-        style={{ width: 500, height: 400 }}
+        ref={webviewRef}
+        originWhitelist={['*']}
+        onMessage={handleMessage}
         javaScriptEnabled={true}
-        >
-
-      </WebView>
-    </View>
+        source={{html: html || ''}}
+        style={{ height: windowHeight, width: width}}
+      />
+    </SafeAreaView>
   );
 }
---
+++ agriculture-korea/scraper.py
@@ -0,0 +1,156 @@
+#scraper for extracting code name pairs for crops in 농사로
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
+def scrape_crop_codes():
+    # Initialize the Chrome driver
+    driver = webdriver.Chrome()
+    driver.get("https://nongsaro.go.kr/portal/farmTechMain.ps?menuId=PS65291&stdPrdlstCode=FC050502")
+    dict = {}
+
+    # Wait for the page to load and the table to be present
+    wait = WebDriverWait(driver, 15)
+    button = wait.until(EC.presence_of_element_located((By.XPATH, "//button[text()='상세검색']")))
+    button.click()
+    category_list = wait.until(EC.presence_of_element_located((By.XPATH, "//ul[@class='cmItem_list']")))
+    cats = category_list.find_elements(By.TAG_NAME, "li")
+    for cat in cats:
+        cat.click()
+        # Wait for the search results to load
+        children = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//ul[@class='cmKind_list']/*")))
+        print([child.get_attribute('id') for child in children])
+        for (index, code) in enumerate([child.get_attribute('id')[10:] for child in children]):
+            dict[code] = children[index].text
+    # Print the dictionary of code-name pairs
+    print(dict)
+    # Close the driver
+    driver.quit()
+    # Save the dictionary to a file
+    with open("crop_codes.txt", "w", encoding="utf-8") as f:
+        for code, name in dict.items():
+            f.write(f"{code}: {name}\n")
+    # Save the dictionary to a JSON file
+    with open("crop_codes.json", "w", encoding="utf-8") as f:
+        json.dump(dict, f, ensure_ascii=False, indent=4)
+
+def scrape_crop_info():
+    try:
+        with open("crop_codes.json", "r", encoding="utf-8") as f:
+            crop_codes = json.load(f)
+    except FileNotFoundError:
+        print("crop_codes.json not found. Please run scrape_crop_codes() first.")
+        return -1
+    res = []
+
+    driver = webdriver.Chrome()
+    i = 0
+    for code, name in crop_codes.items():
+        driver.get(f"https://nongsaro.go.kr/portal/farmTechMain.ps?menuId=PS65291&stdPrdlstCode={code}")
+        wait = WebDriverWait(driver, 5)
+        try:
+            # Wait for the crop information to load
+            cultivars = []
+            try:
+                info_element = wait.until(EC.presence_of_element_located((By.ID, "sectionCrop03")))
+                info_cultivars = info_element.find_elements(By.TAG_NAME, "li")
+                for cultivar in info_cultivars:
+                    cultivar_name = cultivar.find_element(By.TAG_NAME, "strong").get_attribute("textContent").strip()
+                    cultivar_url = cultivar.find_element(By.TAG_NAME, "img").get_attribute("src")
+                    cultivar_use = cultivar.find_element(By.CLASS_NAME, "txt-A").get_attribute("textContent").strip()
+                    cultivar_function = cultivar.find_element(By.CLASS_NAME, "txt-B").get_attribute("textContent").strip()
+                    #print(f"{name} ({code}) - Cultivar: {cultivar_name}, Use: {cultivar_use}, Function: {cultivar_function}")
+                    cultivars.append({
+                        "name": cultivar_name,
+                        "url": cultivar_url,
+                        "use": cultivar_use,
+                        "function": cultivar_function
+                    })
+            except Exception as e:
+                print(f"Error retrieving cultivars for {name} ({code}): {e}")
+            tableData = []
+            try:
+                info_element = driver.find_element(By.ID, "sectionCrop05")
+                tables = info_element.find_elements(By.ID, "nongScheduleTit")
+                for table in tables:
+                    title = table.find_element(By.TAG_NAME, "h5").get_attribute("textContent").strip()
+                    rows = table.find_element(By.TAG_NAME, "tbody").find_elements(By.TAG_NAME, "tr")
+                    ict_ = {
+                        "title": title,
+                        "rows": []
+                    }
+                    for row in rows:
+                        cells = row.find_elements(By.TAG_NAME, "td")
+                        col = 0
+                        ans = []
+                        for cell in cells:
+                            a = cell.get_attribute("textContent").strip()
+                            ict = {}
+                            start_col = col
+                            if cell.get_attribute('colspan') is not None:
+                                colspan = int(cell.get_attribute('colspan'))
+                                col += colspan
+                            else:
+                                col += 1
+                            if len(a) > 0:
+                                ict = {'name': a, 'start_col': start_col, 'end_col': col}
+                                ans.append(ict)
+                        #print(f"{name} ({code}) - {title}: {ans}")
+                        if len(ans) > 0:
+                            ict_["rows"].append(ans)
+                    tableData.append(ict_)
+            except Exception as e:
+                print(f"Error retrieving tables for {name} ({code}): {e}")
+            cards = []
+            try:
+                info_element = driver.find_element(By.ID, "sectionCrop06")
+                info_cards = info_element.find_elements(By.TAG_NAME, "li")
+                for card in info_cards:
+                    card_title = card.find_element(By.CLASS_NAME, "tTxt").get_attribute("textContent").strip()
+                    card_url = card.find_element(By.TAG_NAME, "img").get_attribute("src")
+                    #print(f"{name} ({code}) - Card: {card_title}, URL: {card_url}")
+                    cards.append({
+                        "title": card_title,
+                        "url": card_url
+                    })
+            except Exception as e:
+                print(f"Error retrieving cards for {name} ({code}): {e}")
+            res.append({
+                "code": code,
+                "name": name,
+                "cultivars": cultivars,
+                "tables": tableData,
+                "cards": cards
+            })
+            print(f"Retrieved info for {name} ({code}), {i+1}/{len(crop_codes)}")
+            i += 1
+        except Exception as e:
+            print(f"Error retrieving info for {name} ({code}): {e}, {i+1}/{len(crop_codes)}")
+            i += 1
+    # Close the driver
+    driver.quit()
+    # Save the results to a JSON file
+    with open("crop_info.json", "w", encoding="utf-8") as f:
+        json.dump(res, f, ensure_ascii=False, indent=4)
+    return res
+
+
+if __name__ == "__main__":
+    print('Pick a type of scraping:')
+    print('1. Crop codes')
+    print('2. Crop information')
+    print('3. Quit')
+    choice = input('Enter your choice (1 or 2): ')
+    if choice == '1':
+        scrape_crop_codes()
+    elif choice == '2':
+        res = scrape_crop_info()
+    else:
+        print("Exiting the scraper.")
+        exit(0)
+            
+
+
